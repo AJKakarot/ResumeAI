63c1cf9deba9c4afec20fffd9cdc5af50a433d5c
Ajeet Gupta <gajeet031@gmail.com>
2026-03-22 05:14:33 +0530

    lib: analyzer core (index, parse, extract, analyze)

---
+++ src/lib/analyzer/index.ts
@@ -0,0 +1,8 @@
+export { cleanText } from "./extractText";
+export { parseResume, parseResumeRaw, type ParsedResume } from "./parseResume";
+export { ROLE_SKILLS, type RoleCategory } from "./skillDataset";
+export {
+  analyzeResume,
+  type AnalyzeResult,
+  type AnalyzeOptions,
+} from "./analyzeResume";
