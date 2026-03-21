b25a84d3134d787ea31d8a545383cb9e940dd009
Ajeet Gupta <gajeet031@gmail.com>
2026-03-22 05:14:33 +0530

    chore: Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  // pdf-parse / pdfjs break when webpack bundles them (Object.defineProperty on non-object)
+  serverExternalPackages: ["pdf-parse", "pdfjs-dist", "canvas", "tesseract.js"],
+};
 
 export default nextConfig;
